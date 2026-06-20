6f179a677f577d119f09e2f989cbfd041993f0a7
AdvenRanises <110085960+AdvenRanises@users.noreply.github.com>
2026-06-20 21:49:43 +0800

    Update Configuration.cs

--- Implementation/_Data/_Config/Configuration.cs
+++ Implementation/_Data/_Config/Configuration.cs
@@ -1,82 +1,78 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Xml;
 using System.Xml.Schema;
 
-using Terraria.Plugins.Common;
+namespace HouseRegions
+{
+    public class Configuration
+    {
+        #region Nested: HouseSizeConfig
+        public struct HouseSizeConfig
+        {
+            public int TotalTiles { get; private set; }
+            public int Width { get; private set; }
+            public int Height { get; private set; }
 
-namespace Terraria.Plugins.CoderCow.HouseRegions {
-  public class Configuration {
-    #region [Nested: HouseSizeConfig]
-    public struct HouseSizeConfig {
-      public int TotalTiles { get; private set; }
-      public int Width { get; private set; }
-      public int Height { get; private set; }
+            public static HouseSizeConfig FromXmlElement(XmlElement rootElement)
+            {
+                if (rootElement == null) throw new ArgumentNullException(nameof(rootElement));
 
+                int totalTiles = int.Parse(rootElement["TotalTiles"]!.InnerText);
+                int width = int.Parse(rootElement["Width"]!.InnerText);
+                int height = int.Parse(rootElement["Height"]!.InnerText);
 
-      public static HouseSizeConfig FromXmlElement(XmlElement rootElement) {
-        if (rootElement == null) throw new ArgumentNullException();
+                return new HouseSizeConfig(totalTiles, width, height);
+            }
 
-        int totalTiles = int.Parse(rootElement["TotalTiles"].InnerText);
-        int width = int.Parse(rootElement["Width"].InnerText);
-        int height = int.Parse(rootElement["Height"].InnerText);
+            public HouseSizeConfig(int totalTiles, int width, int height) : this()
+            {
+                TotalTiles = totalTiles;
+                Width = width;
+                Height = height;
+            }
+        }
+        #endregion
 
-        return new HouseSizeConfig(totalTiles, width, height);
-      }
+        public const string CurrentVersion = "1.0";
 
-      public HouseSizeConfig(int totalTiles, int width, int height): this() {
-        this.TotalTiles = totalTiles;
-        this.Width = width;
-        this.Height = height;
-      }
+        public int MaxHousesPerUser { get; set; }
+        public HouseSizeConfig MinSize { get; set; }
+        public HouseSizeConfig MaxSize { get; set; }
+        public bool AllowTShockRegionOverlapping { get; set; }
+        public int DefaultZIndex { get; set; }
+
+        public static Configuration Read(string filePath)
+        {
+            XmlReaderSettings configReaderSettings = new XmlReaderSettings
+            {
+                ValidationType = ValidationType.Schema,
+                ValidationFlags = XmlSchemaValidationFlags.ProcessIdentityConstraints | XmlSchemaValidationFlags.ReportValidationWarnings
+            };
+
+            string configSchemaPath = Path.Combine(Path.GetDirectoryName(filePath)!, Path.GetFileNameWithoutExtension(filePath) + ".xsd");
+            configReaderSettings.Schemas.Add(null, configSchemaPath);
+
+            XmlDocument document = new XmlDocument();
+            using (XmlReader configReader = XmlReader.Create(filePath, configReaderSettings))
+                document.Load(configReader);
+
+            XmlElement rootElement = document.DocumentElement!;
+            string fileVersionRaw = rootElement.HasAttribute("Version") ? rootElement.GetAttribute("Version") : "1.0";
+
+            if (fileVersionRaw != CurrentVersion)
+            {
+                throw new FormatException($"The configuration file is either outdated or too new. Expected version was: {CurrentVersion}. File version is: {fileVersionRaw}");
+            }
+
+            Configuration resultingConfig = new Configuration();
+            resultingConfig.MaxHousesPerUser = int.Parse(rootElement["MaxHousesPerUser"]!.InnerText);
+            resultingConfig.MinSize = HouseSizeConfig.FromXmlElement(rootElement["MinHouseSize"]!);
+            resultingConfig.MaxSize = HouseSizeConfig.FromXmlElement(rootElement["MaxHouseSize"]!);
+            resultingConfig.AllowTShockRegionOverlapping = bool.Parse(rootElement["AllowTShockRegionOverlapping"]!.InnerText);
+            resultingConfig.DefaultZIndex = int.Parse(rootElement["DefaultZIndex"]!.InnerText);
+
+            return resultingConfig;
+        }
     }
-    #endregion
-
-    public const string CurrentVersion = "1.0";
-
-    public int MaxHousesPerUser { get; set; }
-    public HouseSizeConfig MinSize { get; set; }
-    public HouseSizeConfig MaxSize { get; set; }
-    public bool AllowTShockRegionOverlapping { get; set; }
-    public int DefaultZIndex { get; set; }
-
-
-    public static Configuration Read(string filePath) {
-      XmlReaderSettings configReaderSettings = new XmlReaderSettings {
-        ValidationType = ValidationType.Schema,
-        ValidationFlags = XmlSchemaValidationFlags.ProcessIdentityConstraints | XmlSchemaValidationFlags.ReportValidationWarnings
-      };
-
-      string configSchemaPath = Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath) + ".xsd");
-      configReaderSettings.Schemas.Add(null, configSchemaPath);
-      
-      XmlDocument document = new XmlDocument();
-      using (XmlReader configReader = XmlReader.Create(filePath, configReaderSettings))
-        document.Load(configReader);
-
-      // Before validating using the schema, first check if the configuration file's version matches with the supported version.
-      XmlElement rootElement = document.DocumentElement;
-      string fileVersionRaw;
-      if (rootElement.HasAttribute("Version"))
-        fileVersionRaw = rootElement.GetAttribute("Version");
-      else
-        fileVersionRaw = "1.0";
-      
-      if (fileVersionRaw != Configuration.CurrentVersion) {
-        throw new FormatException(string.Format(
-          "The configuration file is either outdated or too new. Expected version was: {0}. File version is: {1}", 
-          Configuration.CurrentVersion, fileVersionRaw
-        ));
-      }
-
-      Configuration resultingConfig = new Configuration();
-      resultingConfig.MaxHousesPerUser = int.Parse(rootElement["MaxHousesPerUser"].InnerText);
-      resultingConfig.MinSize = HouseSizeConfig.FromXmlElement(rootElement["MinHouseSize"]);
-      resultingConfig.MaxSize = HouseSizeConfig.FromXmlElement(rootElement["MaxHouseSize"]);
-      resultingConfig.AllowTShockRegionOverlapping = BoolEx.ParseEx(rootElement["AllowTShockRegionOverlapping"].InnerText);
-      resultingConfig.DefaultZIndex = int.Parse(rootElement["DefaultZIndex"].InnerText);
-
-      return resultingConfig;
-    }
-  }
 }
